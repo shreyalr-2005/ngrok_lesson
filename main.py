d981d1d08b381f91423f0a3cf3ca1acd7caab94b
Shreya LR <shravyashreya26@gmail.com>
2026-02-18 09:34:10 +0530

    do

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import fastapi
+app = fastapi.FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
