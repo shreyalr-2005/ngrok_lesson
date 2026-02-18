4306df10fb71606a54ba9bb2a3813cffe68a35ff
Shreya LR <shravyashreya26@gmail.com>
2026-02-18 11:19:41 +0530

    do

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-import fastapi
-app = fastapi.FastAPI()
+from fastapi import FastAPI
+from routes import user_routes 
+from db import engine, Base
+import models  
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+app.include_router(user_routes.router)
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- models.py
+++ models.py
@@ -5,5 +5,5 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    age = Column(Integer)
+    email = Column(String)
     
